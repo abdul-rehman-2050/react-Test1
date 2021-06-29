138d3ca47f3531f33da7e419dc909820ee8194aa
Abdul Rehman <suermind2002@gmail.com>
2021-06-29 19:19:12 +0500

    topbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+import React  from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Button } from '@material-ui/core';
+import AppbarTop from './components/AppbarTop';
+import theme from './theme'
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
     <div className="App">
+      <AppbarTop/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button color="primary">Hello World</Button>
+
+        
       </header>
     </div>
+    </CssBaseline>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+           500: '#ff9800',
+           contrastText: '#fff',
+           light: '#757ce8',
+           main: '#e65100',
+    
+    },
+      secondary: {
+        main: '#f44336',
+      },
+    },
+
+  })
+  
+
+export default theme
